eec77e265dda1874a2191ae886fc6c99a2d2ede9
Bryan Alves <info.bryanalves@gmail.com>
2020-03-27 13:30:47 -0300

    Criado o layout padrão

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="/css/default.css">    
+    <link rel="stylesheet" href="/css/login.css">
+
+    <title>Hello, world!</title>
+</head>
+
+<body>
+    
+    @yield('content')
+    
+    <script src="/js/app.js"></script>
+    <script src="/js/login.js"></script>
+</body>
+
+</html>
