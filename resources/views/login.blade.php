5905c36dc1e83def32e92add703648542e580c1a
Bryan Alves <info.bryanalves@gmail.com>
2020-03-28 16:07:15 -0300

    Ajuste na estrutura do login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,70 +1,89 @@
-@extends('layout.app')
+<!doctype html>
+<html lang="en">
 
-@section('content')
-<main id="login-page">
-    <div class="login-container">
-        <?php $form = (Session::get('form') == 'signup') ? 'signup' : 'signin'; ?>
-        <div class="login-tabs">
-            <button class="tab-link <?= $form == 'signin' ? 'active' : ''?>"
-                onclick="openTab(event, 'login-signin')">Entrar</button>
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" href="/logo.png" />
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="/css/default.css">
+    <link rel="stylesheet" href="/css/login.css">
 
-            <button class="tab-link <?= $form == 'signup' ? 'active' : ''?>"
-                onclick="openTab(event, 'login-signup')">Cadastre-se</button>
+    <title>PhoneBook</title>
+</head>
+
+<body>
+    <main id="login-page">
+        <div class="login-container">
+            <?php $form = (Session::get('form') == 'signup') ? 'signup' : 'signin'; ?>
+            <div class="login-tabs">
+                <button class="tab-link <?= $form == 'signin' ? 'active' : ''?>"
+                    onclick="openTab(event, 'login-signin')">Entrar</button>
+
+                <button class="tab-link <?= $form == 'signup' ? 'active' : ''?>"
+                    onclick="openTab(event, 'login-signup')">Cadastre-se</button>
+            </div>
+
+            <!-- SignIn -->
+            <div id="login-signin" class="tab-content" style="display:<?= $form == 'signin' ? 'block' : 'none'?>">
+                <div class="login-header">
+                    Digite seu dados de acesso
+                </div>
+                @if (Session::get('form')=='signin')
+                <div class="warning">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+                @endif
+                <div class="login-content">
+                    <form action="/auth" method="POST">
+                        <input type="email" name="signin_email" placeholder="E-mail" value="{{ old('signin_email') }}">
+                        <input type="password" name="password" placeholder="Senha">
+                        {{ csrf_field() }}
+                        <button type="submit">Entrar</button>
+                    </form>
+                </div>
+            </div>
+            <!------------>
+
+            <!-- SignUp -->
+            <div id="login-signup" class="tab-content" style="display:<?= $form == 'signup' ? 'block' : 'none'?>">
+                <div class="login-header">
+                    Informe seu dados para cadastro
+                </div>
+                @if (Session::get('form')=='signup')
+                <div class="warning">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+                @endif
+                <div class="login-content">
+                    <form action="/users" method="POST">
+                        <input type="text" name="name" placeholder="Nome" value="{{ old('name') }}" required>
+                        <input type="email" name="signup_email" placeholder="E-mail" value="{{ old('signup_email') }}"
+                            required>
+                        <input type="password" name="password" placeholder="Senha" required>
+                        <input type="hidden" name="occupation" value="admin">
+                        {{ csrf_field() }}
+                        <button type="submit">Cadastrar</button>
+                    </form>
+                </div>
+            </div>
+            <!------------>
         </div>
+    </main>
 
-        <!-- SignIn -->
-        <div id="login-signin" class="tab-content" style="display:<?= $form == 'signin' ? 'block' : 'none'?>">
-            <div class="login-header">
-                Digite seu dados de acesso
-            </div>
-            @if (Session::get('form')=='signin')
-            <div class="warning">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-            @endif
-            <div class="login-content">
-                <form action="/auth" method="POST">
-                    <input type="email" name="signin_email" placeholder="E-mail" value="{{ old('signin_email') }}">
-                    <input type="password" name="password" placeholder="Senha">
-                    {{ csrf_field() }}
-                    <button type="submit">Entrar</button>
-                </form>
-            </div>
-        </div>
-        <!------------>
+    <?php Session::forget('form'); ?>
+    <script src="https://kit.fontawesome.com/e4bfd8d51e.js" crossorigin="anonymous"></script>
+    <script src="/js/app.js"></script>
+    <script src="/js/login.js"></script>
+</body>
 
-        <!-- SignUp -->
-        <div id="login-signup" class="tab-content" style="display:<?= $form == 'signup' ? 'block' : 'none'?>">
-            <div class="login-header">
-                Informe seu dados para cadastro
-            </div>
-            @if (Session::get('form')=='signup')
-            <div class="warning">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-            @endif
-            <div class="login-content">
-                <form action="/users" method="POST">
-                    <input type="text" name="name" placeholder="Nome" value="{{ old('name') }}" required>
-                    <input type="email" name="signup_email" placeholder="E-mail" value="{{ old('signup_email') }}" required>
-                    <input type="password" name="password" placeholder="Senha" required>
-                    <input type="hidden" name="occupation" value="admin">
-                    {{ csrf_field() }}
-                    <button type="submit">Cadastrar</button>
-                </form>
-            </div>
-        </div>
-        <!------------>
-    </div>
-</main>
-
-<?php Session::forget('form'); ?>
-@endsection
+</html>
