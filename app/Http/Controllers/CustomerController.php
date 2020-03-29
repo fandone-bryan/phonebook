fa96c5e7793cedf884c068240304de7642ae13cc
Bryan Alves <info.bryanalves@gmail.com>
2020-03-29 10:33:13 -0300

    Criação da migration de Clientes e conclusão do cadastro de cliente, junto com as validações

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -3,11 +3,50 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+use App\Customer;
 
 class CustomerController extends Controller
 {
     public function index()
     {
         return view('customer.index');
+    }
+
+    public function create() 
+    {
+        return view('customer.form');
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+        ], [
+            'name.required' => 'Preencha seu nome.',
+            'email.required' => 'Preencha seu e-mail',
+            'email.email' => 'E-mail inválido'
+        ]);
+
+        if ($validator->fails()) {
+            return back()->withInput($request->all)->withErrors($validator);
+        }
+
+        $customerExists = (Customer::where('email', $request->email)->get())->toArray();
+
+        if ($customerExists) {
+            return back()->withInput($request->all)->withErrors(['user_exists' => 'E-mail já cadastrado']);
+        }
+
+        $customer = new Customer();
+
+        $customer->name = $request->name;
+        $customer->email = $request->email;
+
+        $customer->save();
+        
+        return redirect('/');
     }
 }
